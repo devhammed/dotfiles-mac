d185d321267667351231f32190576d0d307c2f96
Hammed Oyedele <itz.harmid@gmail.com>
2025-04-22 11:13:46 +0100

    chore: add files

--- .gnupg/pinentry-ide.sh
+++ .gnupg/pinentry-ide.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-"/Applications/PhpStorm.app/Contents/jbr/Contents/Home/bin/java" -cp "/Applications/PhpStorm.app/Contents/plugins/vcs-git/lib/git4idea-rt.jar:/Applications/PhpStorm.app/Contents/lib/externalProcess-rt.jar" git4idea.gpg.PinentryApp
+if [ -n "$PINENTRY_USER_DATA" ]; then
+  case "$PINENTRY_USER_DATA" in
+    IJ_PINENTRY=*)
+      "/Applications/PhpStorm.app/Contents/jbr/Contents/Home/bin/java" -cp "/Applications/PhpStorm.app/Contents/plugins/vcs-git/lib/git4idea-rt.jar:/Applications/PhpStorm.app/Contents/lib/externalProcess-rt.jar" git4idea.gpg.PinentryApp
+      exit $?
+    ;;
+  esac
+fi
+exec /usr/local/opt/pinentry/bin/pinentry "$@"
